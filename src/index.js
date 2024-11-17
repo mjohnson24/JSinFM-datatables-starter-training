2e3d2c45e3ccd6f4230f67375d8667aa8ea5a4c0
mark johnson <mjohnson24@comcast.net>
2024-11-16 18:31:55 -0600

    get data from filemaker with click event

--- src/index.js
+++ src/index.js
@@ -4,11 +4,32 @@ let table;
 
 window.loadTable = (json) => {
   const obj = JSON.parse(json);
-  // const data = obj.data;
-  const columns = [];
-  const data = [];
+  const data = obj.data;
+  console.log(data);
+  const columns = [
+    { data: "fieldData.CompanyName", title: "Company" },
+    { data: "fieldData.City", title: "City" },
+    { data: "fieldData.State", title: "State", orderable: false },
+    { data: "fieldData.Zip", title: "Zip", orderable: false },
+  ];
+
   table = $("#dtable").DataTable({
     columns,
     data,
+    // paging: false,
+    // ordering: false,
   });
 };
+
+// add a row click event
+$("#dtable").on("click", "tr", function () {
+  const row = table.row(this);
+  console.log(row);
+  const data = row.data();
+  console.log(data);
+  const primaryKey = data.fieldData.PrimaryKey;
+  console.log(primaryKey);
+  const obj = { primaryKey };
+  console.log(obj);
+  FileMaker.PerformScript("Get Data Back", JSON.stringify(obj));
+});
